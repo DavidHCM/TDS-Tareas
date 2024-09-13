4d2bc88588f90220c04fc06e64597ab4c12a3ebd
David Hernández <david.hernandezc@iteso.mx>
2024-09-13 17:53:25 -0600

    removiendo dist

--- Tarea 1/dist/controllers/user.controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userController = void 0;
-class UserController {
-    listAll(req, res) {
-        res.send('Lista de usuarios');
-    }
-}
-exports.userController = new UserController();
